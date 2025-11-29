e01e9d42462cabd1ee7e9b8de82dca498fd68545
princechoudhary5985-design <princechoudhary5985@gmail.com>
2025-11-30 02:19:23 +0530

    Update functions.js

--- file/functions.js
+++ file/functions.js
@@ -1,6 +1,4 @@
-
-
-// variables
+// Responsive reload
 var $win = $(window);
 var clientWidth = $win.width();
 var clientHeight = $win.height();
@@ -13,50 +11,63 @@ $(window).resize(function() {
     }
 });
 
+// Typewriter effect
 (function($) {
-	$.fn.typewriter = function() {
-		this.each(function() {
-			var $ele = $(this), str = $ele.html(), progress = 0;
-			$ele.html('');
-			var timer = setInterval(function() {
-				var current = str.substr(progress, 1);
-				if (current == '<') {
-					progress = str.indexOf('>', progress) + 1;
-				} else {
-					progress++;
-				}
-				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-				if (progress >= str.length) {
-					clearInterval(timer);
-				}
-			}, 75);
-		});
-		return this;
-	};
+    $.fn.typewriter = function() {
+        this.each(function() {
+            var $ele = $(this),
+                str = $ele.html(),
+                progress = 0;
+            $ele.html('');
+            var timer = setInterval(function() {
+                var current = str.substr(progress, 1);
+                if (current == '<') {
+                    progress = str.indexOf('>', progress) + 1;
+                } else {
+                    progress++;
+                }
+                $ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+                if (progress >= str.length) {
+                    clearInterval(timer);
+                }
+            }, 75);
+        });
+        return this;
+    };
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
-	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
-	seconds = seconds % (3600 * 24);
-	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
-	seconds = seconds % 3600;
-	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "Days <span class=\"digit\">" + days + "</span> Hours <span class=\"digit\">" + hours + "</span> Minutes <span class=\"digit\">" + minutes; 
-	$("#clock").html(result);
+// Function to calculate time since birthday
+function timeElapse(date) {
+    var start = new Date(date);
+    var now = new Date();
+    var seconds = Math.floor((now - start) / 1000);
 
-	var text = "THE WORLD JUST GOT LUCKIER SINCE ";
-	$("#message-box").html(text);
+    var days = Math.floor(seconds / (3600 * 24));
+    seconds %= 3600 * 24;
+
+    var hours = Math.floor(seconds / 3600);
+    if (hours < 10) hours = "0" + hours;
+
+    seconds %= 3600;
+    var minutes = Math.floor(seconds / 60);
+    if (minutes < 10) minutes = "0" + minutes;
+
+    var secs = seconds % 60;
+    if (secs < 10) secs = "0" + secs;
+
+    var result = days + " Days <span class='digit'>" + hours + "</span> Hours <span class='digit'>" + minutes + "</span> Minutes <span class='digit'>" + secs + "</span> Seconds";
+    $("#clock").html(result);
+
+    var text = "THE WORLD JUST GOT LUCKIER SINCE 10 DEC 2005 🎂";
+    $("#message-box").html(text);
+}
+
+// Update the clock every second
+setInterval(function() {
+    timeElapse("2005-12-10");
+}, 1000);
+
+// Initial call
+timeElapse("2005-12-10");
 
 }
